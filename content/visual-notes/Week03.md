7ddde546272da9e59a291b7c6155e5441c65421b
JReuben <jennie.susan@gmail.com>
2025-07-09 22:45:21 +0200

    Quartz sync: Jul 9, 2025, 10:45 PM

---
+++ content/visual-notes/Week03.md
@@ -0,0 +1,6 @@
+---
+title: Week 3 of the Visual Note Taking Experiment
+date: 2025-07-08
+draft: false
+tags:
+---
--- sync-to-quartz.sh
+++ sync-to-quartz.sh
@@ -4,7 +4,7 @@ echo "Syncing markdown notes..."
 rsync -av --update --delete ~/Git_Files/Obsidian/Vaults/VTKM-Personal/my-notes/notes/Week*.md ~/Git_Files/my-blog/content/visual-notes
 
 echo "Syncing drawings..."
-rsync -av --update --delete ~/Git_Files/Obsidian/Vaults/VTKM-Personal/my-notes/drawings/ ~/Git_Files/my-blog/quartz/static/drawings/
+rsync -av --update --delete ~/Git_Files/Obsidian/Vaults/VTKM-Personal/my-notes/drawings/ ~/Git_Files/my-blog/content/drawings/
 
 echo "Sync complete!"
 
